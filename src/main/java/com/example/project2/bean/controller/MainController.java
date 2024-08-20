bb5e01908fed88ca48ae6a3a286a01efc8b3ed6f
SungEunWoo <sungeu18@naver.com>
2024-08-21 02:38:31 +0900

    Commit

--- src/main/java/com/example/project2/bean/controller/MainController.java
+++ src/main/java/com/example/project2/bean/controller/MainController.java
@@ -2,16 +2,14 @@ package com.example.project2.bean.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class MainController {
-     @GetMapping("/sbb")
-     @ResponseBody
+     @GetMapping("/A.html")
      public String sbb() {
         System.out.println("index");
-        return "안녕하세요 스프링 부트 게시판에 오신 것을 환영합니다.";
+        return "A";
      }
      
 }
